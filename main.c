5b45db3bf17827f29ea97ef191fb61d8c17ecd41
bhcsayx <gyf161023@gmail.com>
2022-02-10 08:23:57 -0800

    finished source reading

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(int argc, char *argv[]) {
+
+    if(argc < 2) {
+        printf("Usage: pl605 <input file> .\n");
+        exit(-1);
+    }
+
+    const char* dir = argv[1];
+    int input_fd = open(dir, O_RDONLY);
+
+    if(input_fd < 0) {
+        printf("Input file does not exist.\n");
+        exit(-1);
+    }
+
+    struct stat file_meta;
+    fstat(input_fd, &file_meta);
+    off_t size = file_meta.st_size;
+
+    // printf("Input file size: %ld\n", size);
+    char *raw_input = (char*) malloc(size);
+    read(input_fd, raw_input, size);
+
+    // printf("source: %s\n", raw_input);
+
+    exit(0);
+}
